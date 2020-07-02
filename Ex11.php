6cc890ef383e2aac5b38115e98ae1846973bc336
phyuphyusan <phyuphyusan.cu25@gmail.com>
2020-07-02 17:32:11 +0630

    Added a new file

---
+++ Ex11.php
@@ -0,0 +1,21 @@
+<?php
+	$name1="Mr.A";
+	$name2="Mr.B";
+	$name3="Mr.C";
+
+	$salary="1000$";
+	echo "<table border=1>
+			<tr>
+				<td>Salary Of $name1</td>
+				<td>$salary</td>
+			</tr>	
+			<tr>
+				<td>Salary Of $name2</td>
+				<td>$salary</td>
+			</tr>
+			<tr>
+				<td>Salary Of $name3</td>
+				<td>$salary</td>
+			</tr>
+		</table>"	
+?>
---
+++ Ex12.php
@@ -0,0 +1,7 @@
+<?php
+ 	$d='A0';
+ 	echo "Sample Output:";
+	for ($i=0; $i <6 ; $i++) { 
+		echo "<br>$d$i";
+	}
+?>
